be3b8ad9da77d18e33dcf0e7dcaaa6b6f4b6dd71
Vipin     Tekade <tekadevipin@gmail.com>
2020-10-22 16:49:45 +0530

    FaceBook

---
+++ src/com/abc/FbLogin.java
@@ -0,0 +1,10 @@
+package com.abc;
+
+public class FbLogin {
+
+	public static void main(String[] args) {
+		System.out.println("Fb Login Successfully");
+
+	}
+
+}
---
+++ src/com/abc/FbLogout.java
@@ -0,0 +1,10 @@
+package com.abc;
+
+public class FbLogout {
+
+	public static void main(String[] args) {
+		System.out.println("Fb Logout");
+
+	}
+
+}
